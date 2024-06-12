96976bf6cba5d92536260f8f17e317af7b744a58
Sewa Benson <sewabenson@meta.com>
2024-06-12 16:46:52 -0700

    before moving Load Bar Component to App Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import './App.css'
 import Header from './Header';
+import SearchBar from './SearchBar';
 import MovieList from './MovieList';
 import Footer from './Footer';
 
+
+
 function App() {
   return (
-    <>
+    <div classNmae="App">
       <Header/>
       <MovieList/>
       <Footer/>
-    </>
+    </div>
   );
 }
 
--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -1,9 +1,33 @@
+import React, {useState} from 'react';
 import './SearchBar.css'
 
 function SearchBar(){
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const fetchData = async (value) => {
+        const apiKey = import.meta.env.VITE_APP_API_KEY;
+        const response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&page=${moviePage}`);
+        const data = await response.json();
+        const resuults = data.filter[(user) => {
+            return value && user && user.name && user.name.toLowerCase().includes(value);
+        }];
+        console.log(resuults);
+
+    }
+
+
+    const handleChange = (value) => {
+        setSearchTerm(value);
+        fetchData(value);
+    }
+
     return(
         <div id= "search-bar-container">
-            <input className="search-input" />
+            <input
+            placeholder='Type to search...'
+            value = {searchTerm}
+            onChange={(e) => handleChange(e.target.value)}
+            />
             <button className="search-button">Search</button>
         </div>
     )
