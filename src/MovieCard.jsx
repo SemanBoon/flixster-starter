e483297002951e30b758ef3dd7a5ffe023ff58aa
SemanBoon <134331970+SemanBoon@users.noreply.github.com>
2024-06-13 21:17:49 -0700

    Update MovieCard.jsx

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,11 +1,11 @@
 import './MovieCard.css';
 
-function MovieCard({movie, onClick}) {
+function MovieCard({props, onClick}) {
     return(
         <div className="movie-card" onClick={() => onClick(movie)}>
             <img src={props.movieImage} alt="Movie Cover"/>
-            <p>{movie.movieTitle}</p>
-            <p>{movie.movieRating}</p>
+            <p>{props.movieTitle}</p>
+            <p>{props.movieRating}</p>
         </div>
     )
 }
