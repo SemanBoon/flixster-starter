66af095dd740398158821bc7e91f7898097cebb8
SemanBoon <134331970+SemanBoon@users.noreply.github.com>
2024-06-13 20:44:49 -0700

    Updated Modal.jsx

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,27 +1,21 @@
-// import React, { useEffect } from 'react';
-// import '/.Modal.css';
+import React, { useEffect } from 'react';
+import '/.Modal.css';
 
-// const Modal = ({selectedMovie}) => {
-//     useEffect(() => {
-//         return () => {
-//             setisModalOpen(false);
-//         };
-//     }, []);
-//   return (
-//     <>
-//         <div id="my-modal" class="modal-overlay">
-//             <div class="modal-content">
-//                 <h5>{selectedMovie.title}</h5>
-//                 <img src={selectedMovie.poster_path} alt={selectedMovie.title} />
-//                 <p>{selectedMovie.overview}</p>
-//                 <p>Release Date: {selectedMovie.release_date}</p>
-//                 <p>Genre: {selectedMovie.genres.map((genre) => genre.name).join(", ")}</p>
-//             </div>
-
-//             <button>
-//                 <span class = "close-button">close</span>
-//             </button>
-//         </div>
-//     </>
-//   )
-// };
+const Modal = ({movie, onClose}) => {
+    if (!movie) return null;
+  return(
+        <div id="my-modal" class="modal-overlay">
+            <div class="modal-content">
+                <h5>{movie.title}</h5>
+                <img src={selectedMovie.poster_path} alt={selectedMovie.title} />
+                <p>{movie.overview}</p>
+                <p><strong>Release Date:<srong/> {movie.release_date}</p>
+                <p><strong>Genre:</strong> {movie.genre_ids.join(', ')}</p>
+                <p><strong>Genre: </strong>{movie.genres.map((genre) => genre.name).join(", ")}</p>
+            </div>
+            <button>
+                <span class = "close-button" onClick={onClose}>close</span>
+            </button>
+        </div>
+  )
+};
