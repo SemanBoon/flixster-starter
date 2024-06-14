dc1359076c54c84c30c18901c30fd725f59ef359
SemanBoon <134331970+SemanBoon@users.noreply.github.com>
2024-06-13 21:42:01 -0700

    Update Modal.jsx

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect } from 'react';
 import '/.Modal.css';
 
-const Modal = ({movie, onClose}) => {
+function Modal({ movie, onClose }) {
     if (!movie) return null;
   return(
         <div id="my-modal" class="modal-overlay">
             <div class="modal-content">
                 <h5>{movie.title}</h5>
-                <img src={selectedMovie.poster_path} alt={selectedMovie.title} />
+                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={selectedMovie.title} />
                 <p>{movie.overview}</p>
-                <p><strong>Release Date:<srong/> {movie.release_date}</p>
+                <p><strong>Release Date:<strong/> {movie.release_date}</p>
                 <p><strong>Genre:</strong> {movie.genre_ids.join(', ')}</p>
                 <p><strong>Genre: </strong>{movie.genres.map((genre) => genre.name).join(", ")}</p>
             </div>
